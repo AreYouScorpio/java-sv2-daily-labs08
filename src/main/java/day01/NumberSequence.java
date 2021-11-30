8f6fba8dd7bd7637f2718d537354c0820321560b
AreYouScorpio <akos.schweger@gmail.com>
2021-11-30 15:16:07 +0100

    Initial commit

---
+++ src/main/java/day01/NumberSequence.java
@@ -0,0 +1,27 @@
+package day01;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumberSequence {
+
+    List<Integer> numbers = new ArrayList<>();
+    int piece;
+    int min;
+    int max;
+
+
+    public NumberSequence(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+
+
+
+    closeToAverage(int value) {
+    List<Integer> collected = new ArrayList<>();
+
+    }
+
+
+
+}
