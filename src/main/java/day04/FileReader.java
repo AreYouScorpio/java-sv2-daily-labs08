2dc49448628c44b621fb34d347e57ebdecfbd272
AreYouScorpio <akos.schweger@gmail.com>
2021-12-03 23:51:22 +0100

    foci házi task added

--- src/main/java/day04/FileReader.java
+++ src/main/java/day04/FileReader.java
@@ -12,8 +12,41 @@ public class FileReader {
 
     public static void main(String[] args) {
         int min = new FileReader().findSmallestTemperatureSpread("weather.dat");
+        String chosenClub = new FileReader().findSmallestDifference("football.dat");
         System.out.println(min);
+        System.out.println(chosenClub);
     }
+
+
+    private String findSmallestDifference(String filename){
+        List<String> lines;
+        try {
+            lines = Files.readAllLines(Paths.get("src/main/resources/" + filename));
+
+
+            int minSpread = 1000;
+            String club = null;
+            for (int i = 1; i < lines.size() - 5; i++) {
+                String team = lines.get(i).substring(7, 21).trim();
+                int F = Integer.parseInt(lines.get(i).substring(43, 45).trim());
+                int A = Integer.parseInt(lines.get(i).substring(50, 52).trim());
+                //System.out.println(F);
+                //System.out.println(A);
+                int spread2 = Math.abs(F - A);
+                if (minSpread > spread2) {
+                    minSpread = spread2;
+                    club = team;
+
+                }
+            }
+            return club;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new IllegalStateException("Can not read file", e);
+        }
+    }
+
     private int findSmallestTemperatureSpread(String filename) {
 
         List<String> lines;
