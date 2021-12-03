395e45dade3f1ead51dba1659505db940104fabd
AreYouScorpio <akos.schweger@gmail.com>
2021-12-03 11:15:19 +0100

    coordSystem added

---
+++ src/test/java/day02/CoordinateSystem.java
@@ -0,0 +1,36 @@
+package day02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CoordinateSystem {
+
+    private final List<Point> points;
+
+    public CoordinateSystem(List<Point> points) {
+        this.points = points;
+    }
+
+    public List<Point> getPoints() {
+        // return new ArrayList<>(points); - igy x 2 lesz
+        // igy meg 1 ha ezt írom:
+        List<Point> result = new ArrayList<>();
+        for (Point p : points) {
+            result.add(new Point(p));
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+
+        Point p = new Point(1, 2);
+
+        CoordinateSystem coordinateSystem = new CoordinateSystem(new ArrayList<>(List.of(p)));
+
+        coordinateSystem.getPoints().get(0).setX(2);
+        System.out.println(p.getX());
+
+    }
+
+}
